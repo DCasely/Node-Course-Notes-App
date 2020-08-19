a4bb462637836d5278c8dd94568fe7e50c70da5b
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-08-19 16:14:39 -0400

    argument parsing with yargs

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-const command = process.argv[2];
+// Customize yargs verfsion
+yargs.version('1.1.0');
 
-console.log(process.argv[3]);
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log('Adding a new note!');
+  },
+});
 
-if (command === 'add') console.log('Adding note');
-if (command === 'remove') console.log('Removing note');
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a new note',
+  handler: function () {
+    console.log('Removing a new note!');
+  },
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a new note',
+  handler: function () {
+    console.log('Reading a new note!');
+  },
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List a new note',
+  handler: function () {
+    console.log('Listing a new note!');
+  },
+});
+
+console.log(yargs.argv);
